3c00206c9e4d23d5c44c35827e74926dda941c06
ApolloSolo <matt1teixeira@gmail.com>
2022-05-30 13:28:14 -0400

    Updated README.md

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -3,7 +3,7 @@ import LaptopImg from "../assets/laptop.jpg";
 
 function Analytics() {
   return (
-    <div className="w-full bg-white px-4 text-black">
+    <div className="w-full bg-white py-16 px-4 text-black">
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
         <img className="w-[500px] mx-auto my-4" src={LaptopImg} alt="laptop" />
         <div className="flex flex-col justify-center">
