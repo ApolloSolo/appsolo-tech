ea75c16e95d130d563c554b13394a4abaf21b896
ApolloSolo <matt1teixeira@gmail.com>
2022-08-21 15:43:18 -0400

    Mod hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,8 +23,8 @@ const Navbar = () => {
       <div onClick={handleNav} className='block md:hidden'>
           {!nav ? <AiOutlineClose size={25}/> : <AiOutlineMenu size={25} />}
       </div>
-      <div className={!nav ? "fixed left-0 top-0 w-[60%] border-r border-r-gray-600 h-full bg-gray-800 ease-in-out duration-500" : 'fixed left-[-100%]'}>
-        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">
+      <div className={!nav ? "fixed left-0 top-0 w-[60%] border-r border-r-gray-600 h-full bg-gray-800 ease-in-out duration-500" : 'fixed top-0 w-[60%] left-[-100%] h-full ease-in-out duration-500'}>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] text-center mt-4">
           AppSolo Tech.
         </h1>
         <ul className="uppercase p-4">
